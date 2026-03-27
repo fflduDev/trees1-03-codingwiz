d36e74e0f580a6a3a0f2035a503c7eac04997e12
Logan Ameres <logan.ameres@student.fairfield.edu>
2026-03-27 11:48:48 -0400

    added methods in OrgChartImpl

--- OrgChartImpl.java
+++ OrgChartImpl.java
@@ -7,6 +7,35 @@ public class OrgChartImpl implements OrgChart{
 
 	//Employee is your generic 'E'..
 	private List<GenericTreeNode<Employee>> nodes = new ArrayList<>();
-	
-	
+
+
+	@Override
+	public void addRoot(Employee e) {
+		
+	}
+
+	@Override
+	public void clear() {
+
+	}
+
+	@Override
+	public void addDirectReport(Employee manager, Employee newPerson) {
+
+	}
+
+	@Override
+	public void removeEmployee(Employee firedPerson) {
+
+	}
+
+	@Override
+	public void showOrgChartDepthFirst() {
+
+	}
+
+	@Override
+	public void showOrgChartBreadthFirst() {
+
+	}
 }
